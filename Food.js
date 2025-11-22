3315558ea2518af3292847e022d60c97157e3dad
Vivek Singathi <viv.singathi@gmail.com>
2025-11-21 22:35:20 -0500

    stuff

---
+++ Food.js
@@ -0,0 +1,10 @@
+const keys = ["q", "e"];
+const foodTypes = [];
+
+class Food {
+    constructor(game, key, type){
+        this.game = game;
+        this.key = key;
+        this.type = type;
+    }
+}
--- main.js
+++ main.js
@@ -3,13 +3,20 @@ class Game {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
     }
+
+    printStuff() {
+        console.log("wwww");
+    }
+
 }
 
 window.addEventListener('load', loadGame);
 
 function loadGame(){
-    const canvas = document.getElementById("gameFrame")
-    
+    const canvas = document.getElementById("gameFrame");
+    const game = new Game(canvas);
+    game.printStuff();
+
 }
 
 
